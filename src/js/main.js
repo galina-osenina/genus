351d1a39109e9e86e2aa53061885227ac198f321
galina osenina <galina.osenina@gmail.com>
2020-07-01 12:13:24 +0300

    form=placeholder script

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,14 @@ $(function() {
     $('.header__nav').toggleClass('header__nav_visible');
     $('body').toggleClass('body_fixed');
   });
+
+  $('.js-form-control').on('change focus', function(){
+    $(this).parent().find('.form__placeholder').addClass('form__placeholder--active');
+  });
+
+  $('.js-form-control').on('blur', function(){
+    if (!$(this).val()){
+      $(this).parent().find('.form__placeholder').removeClass('form__placeholder--active');
+    }
+  });
 });
