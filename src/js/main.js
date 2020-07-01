dd868774c1365a7d2a5b695926eb686847505ff5
galina osenina <galina.osenina@gmail.com>
2020-07-01 12:11:10 +0300

    beginning of footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 //= ../../node_modules/@fortawesome/fontawesome-free/js/all.js
+//= ../../node_modules/jquery/dist/jquery.min.js
 
-
+$(function() {
+  $('.menu-button').on('click', function () {
+    $(this).toggleClass('menu-button_open');
+    $('.header__nav').toggleClass('header__nav_visible');
+    $('body').toggleClass('body_fixed');
+  });
+});
