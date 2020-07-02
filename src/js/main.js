13ebd1f141f3ee1677996c5dc46d5edd20517cd6
Galina <osenina.galina@gmail.com>
2020-07-02 15:35:36 +0300

    rest of pages

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,12 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
 
 $(function() {
+
+
+    $('.header__nav-list').find('.nav-list__link').each(function() {
+        $(this).toggleClass('nav-list__link_active', $(this).attr('href') == window.location.pathname);
+    });
+
   $('.menu-button').on('click', function () {
     $(this).toggleClass('menu-button_open');
     $('.header__nav').toggleClass('header__nav_visible');
@@ -17,4 +23,16 @@ $(function() {
       $(this).parent().find('.form__placeholder').removeClass('form__placeholder--active');
     }
   });
+
+  $('.js-contact-btn').on('click', function (e) {
+      var _target = $('.js-form-contact')
+
+      if( _target.length ) {
+          e.preventDefault();
+          $('html, body').animate({
+              scrollTop: _target.offset().top
+          }, 500);
+      }
+  });
+
 });
